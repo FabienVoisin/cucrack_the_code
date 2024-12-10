81800b1dd899e9576807bdbad510c2945a9b77a1
Ubuntu <ubuntu@ip-10-0-3-80.ap-southeast-2.compute.internal>
2024-12-10 13:24:07 +0000

    Added GPU instructions for swapable check and place it into the player class

--- src/gpuutilsfunc.cu
+++ src/gpuutilsfunc.cu
@@ -35,27 +35,59 @@ __host__ unsigned int* allocate_device_memory(unsigned int numelements){
     return devicedata;
 }
 
-__host__ void codecheck(unsigned int *playercode,unsigned int *constcode){
+
+__host__ void codecheck(player &player,unsigned int *constcode){
     unsigned int blockspergrid=CODESIZE/THREADSPERBLOCK+1;
     unsigned int *devicecheck; //pointer to device hot encoding must return the index of invalid
+    unsigned int *deviceswap;
     unsigned int *devicecode;
-    unsigned int host_incorrect_numbers=0;
-    unsigned int *device_incorrect_numbers;
+    unsigned int host_incorrect_number=0;
+    unsigned int host_swapable_number=0;
+    unsigned int *device_incorrect_number;
     unsigned int *device_incorrect_array;
+    unsigned int *device_swapable_number;
+    unsigned int *device_swapable_array;
     
     devicecheck=allocate_device_memory(CODESIZE);
     devicecode=allocate_device_memory(CODESIZE);
-    device_incorrect_numbers=allocate_device_memory(sizeof(unsigned int));
-    copy_input_to_device(playercode,devicecode,CODESIZE);
-    copy_input_to_device(&host_incorrect_numbers,device_incorrect_numbers,CODESIZE,sizeof(unsigned int));
-    /*Now we need to create a new array tp store non zero values*/
-    gpu_device_check<<<blockspergrid,THREADSPERBLOCK>>>(devicecode,constcode,devicecheck,incorrectnumbers,CODESIZE);
-    copy_device_to_output(device_incorrect_numbers,&host_incorrect_numbers,sizeof(int));
-    device_incorrect_array=allocate_device_memory(host_incorrect_numbers);
-    //I somehow need to push the zeros to the side so I can later use to resuce the array
-    incorrect_num_threads=max(host_incorrect_numbers,32); //32 being the adequate number of threads
+    deviceswap=allocate_device_memory(CODESIZE);
 
+    device_incorrect_number=allocate_device_memory(sizeof(unsigned int));
+    device_swapable_number=allocate_device_memory(sizeof(unsigned int));
+
+    copy_input_to_device(player->currentcodeattempt,devicecode,CODESIZE);
+    copy_input_to_device(&host_incorrect_number,device_incorrect_number,sizeof(unsigned int));
+    copy_input_to_device(&host_swapable_number,device_swapable_number,sizeof(unsigned int));
+
+    /*Now we need to create a new array tp store non zero values*/
+    gpudevicecheckincorrect<<<blockspergrid,THREADSPERBLOCK>>>(devicecode,constcode,devicecheck,device_incorrect_number,CODESIZE);
+    copy_device_to_output(device_incorrect_number,&host_incorrect_number,sizeof(int));
+    device_incorrect_array=allocate_device_memory(host_incorrect_number);
+    //I somehow need to push the zeros to the side so I can later use to resuce the array
+    int incorrect_num_threads=max(host_incorrect_number,32); //32 being the adequate number of threads
+    int incorrect_num_blocks=host_incorrect_number/incorrect_num_threads + 1 ; 
+
+    place_incorrect_swap_value<<<incorrect_num_blocks,incorrect_num_threads>>>(devicecheck,device_incorrect_array, device_incorrect_number);
+    /*Place the data back into the player values*/
+    /*I will need to free these values before reinitialize them*/
+    player.flagincorrect=new unsigned int[host_incorrect_numbers];
+    copy_device_to_output(device_incorrect_number,&host_incorrect_number,sizeof(int));
     
+    copy_device_to_output(device_incorrect_array,player.flagincorrect,host_incorrect_numbers*sizeof(unsigned int));
+
+    gpudevicecheckswap<<<blockspergrid,THREADSPERBLOCK>>>(devicecode,constcode,deviceswapcheck,device_swapable_number,CODESIZE);
+    copy_device_to_output(device_swapable_number)
+    int incorrect_num_threads=max(host_incorrect_number,32); //32 being the adequate number of threads
+    int incorrect_num_blocks=host_incorrect_number/incorrect_num_threads + 1 ; 
+    place_incorrect_swap_value<<<incorrect_num_blocks,incorrect_num_threads>>>(devicecheck,device_swapable_array,device_swapable_number);
+    player.flagswap=new unsigned int[host_swapable_number];
+    copy_device_to_output(device_swapable_array,player.flagswap,host_swapable_number*sizeof(unsigned int));
+    /* I need to free all pointers and CUDA variables*/
+    cudafree(devicecheck);
+    cudafree(devicecode);
+    cudafree(deviceswap);
+    cudafree(device_incorrect_number);
+    cudafree(device_swapable_number);
 }
 
 /*What do I need to do?
@@ -82,7 +114,7 @@ __device__ void get_key_number_values(unsigned int *devicecheck, int index,unsig
     }
 }
 
-__global__ void place_incorrect_swap_values(unsigned int *devicecheck,unsigned int *device_incorrect_array, int numelements, unsigned int numthreads){
+__global__ void place_incorrect_swap_values(unsigned int *devicecheck,unsigned int *device_incorrect_array, int numelements){
     int blockId=blockIdx.z*(gridDim.x*gridDim.y)+blockIdx.y*(gridDim.x)+blockIdx.x;
     int startindex=blockId*(blockDim.x*blockDim.y*blockDim.z)+threadIdx.z*(blockDim.x*blockDim.y)+threadIdx.y*(blockDim.y)+threadIdx.x;
     int index=startindex;
@@ -94,7 +126,7 @@ __global__ void place_incorrect_swap_values(unsigned int *devicecheck,unsigned i
     __syncthreads;
 }
 
-__global__ void gpudevicecheckincorrect(unsigned int *devicecode, unsigned int *constcode, unsigned int *deviceswapcheck, unsigned int *incorrectswapnumber,int numelements){
+__global__ void gpudevicecheckswap(unsigned int *devicecode, unsigned int *constcode, unsigned int *deviceswapcheck, unsigned int *incorrectswapnumber,int numelements){
     int blockId=blockIdx.z*(gridDim.x*gridDim.y)+blockIdx.y*(gridDim.x)+blockIdx.x;
     int index=blockId*(blockDim.x*blockDim.y*blockDim.z)+threadIdx.z*(blockDim.x*blockDim.y)+threadIdx.y*(blockDim.y)+threadIdx.x;
 
